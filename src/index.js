34583229249e6a1ae8f90ade0157a9f48e5d4f37
Gabriel da Silva Maciel <136171956+GabrielMacielDxD@users.noreply.github.com>
2023-11-16 14:27:58 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+var listaFilmesAnimes = ["https://optclean.com.br/wp-content/uploads/2018/01/ed3b77f7-9fb9-4659-9842-a8eaca0d6c31.jpg",'https://www.cafecomfilme.com.br/media/k2/items/cache/e65a3cb5bf8ddad3661b23ae64e6228c_XL.jpg?t=1499545054',"https://1.bp.blogspot.com/-ivvFa9z173A/WpDC39g8e0I/AAAAAAAAV-0/vyk0FonlysgkBovsQhPM8LRuvFyssOuNgCLcBGAs/s1600/A-chegad-the-arrival.jpg","https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080","https://pbs.twimg.com/media/EVP0f8iUwAIjf4w?format=jpg&name=4096x4096","https://i.pinimg.com/originals/7d/b5/4f/7db54f80534978b7ad6cfde29e0c363e.jpg","https://cdn.fstatic.com/media/movies/covers/2020/08/funeste_xmIRmw0.jpg","https://www.ecranlarge.com/media/cache/1600x1200/uploads/image/001/120/lcczvb2ynxg7joqgvigwz3l66l-950.jpg","https://assistirfilmesonlinehd12.com/wp-content/uploads/2023/05/ttkTwdSx21AB1EDastR6xwMC2PT.jpg","https://metagalaxia.com.br/wp-content/uploads/2019/05/vingadores-ultimato-resenha-poster.jpg","https://media.senscritique.com/media/000019871642/source_big/High_School_Dx_D_Hero.jpg","https://uploads.jovemnerd.com.br/wp-content/uploads/2023/04/transformers__56e20u.jpg","https://queanime.com/wp-content/uploads/2019/09/tate_no_yuusha_2_3-696x1044.jpg","https://i0.wp.com/viventeandante.com/wp-content/uploads/2021/11/novo-homem-aranha-de-volta-para-casa.jpg?resize=768%2C960&ssl=1"];
+
+//  listaFilmesAnimes [0] = "https://optclean.com.br/wp-content/uploads/2018/01/ed3b77f7-9fb9-4659-9842-a8eaca0d6c31.jpg"
+//   listaFilmesAnimes [1] = 'https://www.cafecomfilme.com.br/media/k2/items/cache/e65a3cb5bf8ddad3661b23ae64e6228c_XL.jpg?t=1499545054'
+//   listaFilmesAnimes [2] = "https://1.bp.blogspot.com/-ivvFa9z173A/WpDC39g8e0I/AAAAAAAAV-0/vyk0FonlysgkBovsQhPM8LRuvFyssOuNgCLcBGAs/s1600/A-chegad-the-arrival.jpg"
+//   listaFilmesAnimes [3] = "https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080"
+//  listaFilmesAnimes [4] = "https://pbs.twimg.com/media/EVP0f8iUwAIjf4w?format=jpg&name=4096x4096"
+//   listaFilmesAnimes [5] = "https://i.pinimg.com/originals/7d/b5/4f/7db54f80534978b7ad6cfde29e0c363e.jpg"
+
+
+for(var i=0; i < listaFilmesAnimes.length; i++) {
+    document.write("<img src=" + listaFilmesAnimes[i] + ">");
+}
+
+
+
+
+
+
